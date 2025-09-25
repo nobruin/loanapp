0f26e2a825f1bd8f1dcbc4a35b464330046bff5b
nobruin <nobruin@gmail.com>
2025-09-25 12:14:42 -0300

    feat: remove identity context

--- src/main/kotlin/com/loanapp/identity/api/v1/dto/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.loanapp.identity.api.v1.dto
-
-data class LoginRequest(
-    val email: String,
-    val password: String,
-)
--- src/main/kotlin/com/loanapp/identity/infra/persistence/AuthUserJpaRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.loanapp.identity.infra.persistence
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AuthUserJpaRepository : JpaRepository<AuthUserEntity, String> {
-    fun findByEmail(email: String): AuthUserEntity?
-}
